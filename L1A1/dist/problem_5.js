774583300dbedaad46c1abe49d11ec24efd5a12a
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-11-07 19:49:22 -0800

    solve problem 5

---
+++ L1A1/dist/problem_5.js
@@ -0,0 +1,8 @@
+"use strict";
+{
+    function getProperty(obj, key) {
+        return obj[key];
+    }
+    const person = { name: "Alice", age: 30, job: "Engineer" };
+    console.log(getProperty(person, "name"));
+}
