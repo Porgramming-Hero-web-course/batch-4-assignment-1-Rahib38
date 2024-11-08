7b6c5bae33f2478707e2fb33eccd91e1206135fb
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-11-08 11:56:57 -0800

    solve

--- L1A1/dist/problem-1.js
+++ L1A1/dist/problem-1.js
@@ -4,5 +4,5 @@
         const sum = num.reduce((total, value) => total + value, 0);
         return sum;
     }
-    console.log(sumArray([1, 2, 3, 4, 5]));
+    // console.log(sumArray([1, 2, 3, 4, 5]));
 }
--- L1A1/src/problem-1.ts
+++ L1A1/src/problem-1.ts
@@ -4,5 +4,5 @@
     return sum;
   }
 
-  console.log(sumArray([1, 2, 3, 4, 5]));
+  // console.log(sumArray([1, 2, 3, 4, 5]));
 }
