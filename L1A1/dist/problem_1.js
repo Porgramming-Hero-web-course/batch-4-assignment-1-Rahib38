8b675111461211bb1ad9560b00db61321a4b2b85
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-11-07 16:19:10 -0800

    solve problem-1

---
+++ L1A1/dist/problem_1.js
@@ -0,0 +1,11 @@
+"use strict";
+{
+    function sumArray(num) {
+        let sum = 0;
+        for (let i = 0; i < num.length; i++) {
+            sum += num[i];
+        }
+        return sum;
+    }
+    //   console.log(sumArray([1, 2, 3, 4, 5]));
+}
---
+++ L1A1/src/problem_1.ts
@@ -0,0 +1,11 @@
+{
+  function sumArray(num: number[]): number {
+    let sum = 0;
+    for (let i = 0; i < num.length; i++) {
+      sum += num[i];
+    }
+    return sum;
+  }
+
+  //   console.log(sumArray([1, 2, 3, 4, 5]));
+}
